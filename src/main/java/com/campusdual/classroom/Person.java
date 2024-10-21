4c4c44c93c116a2095fa7da4e420a8922e4bdc29
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-21 15:51:04 +0200

    Ejercicio14acabado

--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -4,8 +4,30 @@ public class Person {
 	protected String name;
 	protected String surname;
 
+
 	public Person(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public void getDetails(){
+		System.out.println("Name: " + this.name + " Surname: " + this.surname);
 	}
 }
